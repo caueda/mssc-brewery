00fee7fb51526014f60e27071d43789c261510ac
Carlos R. Ueda <carlos.ueda@gmail.com>
2023-03-21 07:18:02 -0300

    Creating a converter for mapstruct Creating a converter mapper that converts Timestamp and OffsetDateTime and vice versa

--- src/main/java/com/devueda/msscbrewery/web/mappers/BeerMapper.java
+++ src/main/java/com/devueda/msscbrewery/web/mappers/BeerMapper.java
@@ -4,7 +4,7 @@ import com.devueda.msscbrewery.domain.Beer;
 import com.devueda.msscbrewery.web.model.BeerDto;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(uses = {DateMapper.class})
 public interface BeerMapper {
     BeerDto beerToBeerDto(Beer beer);
     Beer beerDtoToBeer(BeerDto beerDto);
--- src/main/java/com/devueda/msscbrewery/web/model/BeerDto.java
+++ src/main/java/com/devueda/msscbrewery/web/model/BeerDto.java
@@ -8,6 +8,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
 @Data
@@ -23,4 +24,7 @@ public class BeerDto {
     private String beerStyle;
     @Positive
     private Long upc;
+
+    private OffsetDateTime createdDate;
+    private OffsetDateTime lastUpdatedDate;
 }
